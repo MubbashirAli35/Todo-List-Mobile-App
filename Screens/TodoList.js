6676b94871435573c8125bf2b7aec65034323c93
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-11 03:32:17 +0500

    Todo Detail Overlay/Modal added

--- Screens/TodoList.js
+++ Screens/TodoList.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { View, ActivityIndicator, FlatList, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native';
+import { ListItem, Overlay } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
@@ -9,6 +9,10 @@ import { fetchTodos } from '../redux/ActionCreators';
 const TodoList = () => {
     const user = useSelector(state => state.users);
     const todos = useSelector(state => state.todos);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [todoVisible, setTodoVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -16,7 +20,7 @@ const TodoList = () => {
             dispatch(fetchTodos(user.token));
         
         console.log('Token: ' + user.token);
-    }, [user.token])
+    }, [user.token]);
 
     const renderListItem = ({ item }) => {
         return(
@@ -24,14 +28,19 @@ const TodoList = () => {
                     containerStyle={{ backgroundColor: '#309fba', marginBottom: 10 }}
                     titleStyle={{ color: '#ffffff' }}
                                     rightIcon={() => { return(item.isCompleted ? <FontAwesome5 
-                                                                                    name='check-circle'
-                                                                                    solid={true}
-                                                                                    color='#ffffff'
-                                                                                    size={16} /> : 
-                                                                                    <FontAwesome5 name='times-circle'
-                                                                                    solid={true}
-                                                                                    color='#ffffff'
-                                                                                    size={16} /> ) }} />);
+                                                        name='check-circle'
+                                                        solid={true}
+                                                        color='#ffffff'
+                                                        size={16} /> : 
+                                                        <FontAwesome5 name='times'
+                                                        solid={true}
+                                                        color='#f70000'
+                                                        size={16} /> ) }}
+                    onPress={() => {
+                        setTitle(item.title);
+                        setDescription(item.description);
+                        setTodoVisible(!todoVisible);
+                    }} />);
     } 
 
     if(user.isLoading || todos.isLoading) {
@@ -50,6 +59,17 @@ const TodoList = () => {
                         renderItem={renderListItem}
                         key={item => item._id} />  }
                </View>
+
+               <Overlay isVisible={visible} 
+                    onBackdropPress={() => setVisible(!visible)}
+                    overlayStyle={styles.overlayStyle}>
+                    <Text style={{ fontSize: 22 }}>
+                        {title}
+                    </Text>
+                    <Text>
+                        {description}
+                    </Text>
+               </Overlay>
             </View>
         )
     }
@@ -59,6 +79,11 @@ const styles = StyleSheet.create({
     list: {
         height: '90%',
         width: '90%', 
+    },
+    overlayStyle: {
+        flexDirection: 'column', 
+        alignItems: 'center',
+        justifyContent: 'space-between'
     }
 });
 
