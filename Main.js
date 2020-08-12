7bbd694be6805e60f07be307389779714abd3d8a
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-12 21:17:56 +0500

    Todo Appbar changed

--- Main.js
+++ Main.js
@@ -18,9 +18,10 @@ function Main() {
         return(
             <TodoStackNavigator.Navigator initialRouteName='TodoList'
                 screenOptions={{ headerTitle: 'Todos', 
-                    headerRight: () => {return <FontAwesome5 name='pen-fancy' 
+                    headerRight: () => {return <FontAwesome5 name='feather-alt' 
                                                 style={{ paddingRight: 20 }} 
-                                                size={18} 
+                                                size={18}
+                                                solid={true} 
                                                 onPress={() => navigation.navigate('AddTodo')} />}}}>
                 <TodoStackNavigator.Screen name='TodoList' component={TodoList} />
             </TodoStackNavigator.Navigator>
