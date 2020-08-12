745d494a724e95c19e476cdd08ab407d7bf24ad1
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-12 18:36:52 +0500

    Client Side Edit/Delete Todo functionality added

--- Screens/TodoList.js
+++ Screens/TodoList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native';
-import { ListItem, Overlay } from 'react-native-elements';
+import { ListItem, Overlay, Divider, Button } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
@@ -13,6 +13,7 @@ const TodoList = () => {
     const [description, setDescription] = useState('');
     const [todoVisible, setTodoVisible] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
+    const [completed, setCompleted] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -25,7 +26,7 @@ const TodoList = () => {
     const renderListItem = ({ item }) => {
         return(
             <ListItem title={item.title} 
-                    containerStyle={{ backgroundColor: '#309fba', marginBottom: 10 }}
+                    containerStyle={{ backgroundColor: '#309fba', marginBottom: 10, borderRadius: 20 }}
                     titleStyle={{ color: '#ffffff' }}
                                     rightIcon={() => { return(item.isCompleted ? <FontAwesome5 
                                                         name='check-circle'
@@ -40,7 +41,9 @@ const TodoList = () => {
                         setTitle(item.title);
                         setDescription(item.description);
                         setTodoVisible(!todoVisible);
-                    }} />);
+                        item.isCompleted ? setCompleted(true) : setCompleted(false);
+                    }}
+                    onLongPress={() => setModalVisible(!modalVisible)} />);
     } 
 
     if(user.isLoading || todos.isLoading) {
@@ -60,15 +63,58 @@ const TodoList = () => {
                         key={item => item._id} />  }
                </View>
 
-               <Overlay isVisible={visible} 
-                    onBackdropPress={() => setVisible(!visible)}
+               <Overlay isVisible={todoVisible} 
+                    onBackdropPress={() => setTodoVisible(!todoVisible)}
                     overlayStyle={styles.overlayStyle}>
                     <Text style={{ fontSize: 22 }}>
-                        {title}
+                        Title: {title}
                     </Text>
-                    <Text>
-                        {description}
+                    <Divider style={{ height: 1, 
+                            backgroundColor: '#e1e8ee', 
+                            alignSelf: 'stretch', 
+                            marginBottom: 10 }} />
+
+                    <Text style={{ marginBottom: 10 }}>
+                        Description: {description}
                     </Text>
+
+                    {
+                        completed ?
+                        <View style={{ flexDirection: 'row' }}>
+                            <FontAwesome5 name='check'
+                                color='#00ff00'
+                                solid={true}
+                                size={16} />
+                            <Text style={{ marginLeft: 4 }}>
+                                Completed!
+                            </Text>
+                        </View> :
+                        <View style={{ flexDirection: 'row' }}>
+                            <FontAwesome5 name='times'
+                                color='#ff0000'
+                                solid={true}
+                                size={16} />
+                            <Text style={{ marginLeft: 6, color: '#ff0000' }}>
+                                Not Completed!
+                            </Text>
+                        </View>
+                    }
+               </Overlay>
+
+               <Overlay isVisible={modalVisible}
+                        onBackdropPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={{ fontSize: 16, marginBottom: 10 }} >
+                        Wanna edit or delete this todo?
+                    </Text>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Button title='Delete'
+                            buttonStyle={{ borderRadius: 20, paddingHorizontal: 10 }} />
+                        <Button title='Edit'
+                            buttonStyle={{ paddingHorizontal: 18, borderRadius: 20 }} />
+                        <Button title='Cancel'
+                             buttonStyle={{ borderRadius: 20, paddingHorizontal: 10 }}
+                             onPress={() => setModalVisible(!modalVisible)} />
+                    </View>
                </Overlay>
             </View>
         )
@@ -83,7 +129,7 @@ const styles = StyleSheet.create({
     overlayStyle: {
         flexDirection: 'column', 
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
     }
 });
 
